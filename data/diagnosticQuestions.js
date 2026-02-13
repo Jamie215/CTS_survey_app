3b92ed77b6a972d1898211119e7be132fcd6198e
Jamie Kim <jmkim0603@gmail.com>
2026-02-13 14:05:54 -0500

    Update diagnosticQuestions.js

--- data/diagnosticQuestions.js
+++ data/diagnosticQuestions.js
@@ -6,10 +6,10 @@
     { id: 4, number: '1d', text: "Do you have any quick movements or positions that relieve your tingling or numbness?", requiresNumbnessOrTingling: true },
     { id: 5, number: '1e', text: "Do you have numbness or tingling in your little (small/pinky) finger?", requiresNumbnessOrTingling: true },
     { id: 6, number: '1f', text: "Do certain activities (for example, holding objects or repetitive finger movement) increase the numbness or tingling in your fingers?", requiresNumbnessOrTingling: true },
-    { id: 7, number: '1g', text: "Did you have numbness or tingling in your fingers when you were pregnant? (If relevant)", hasNotRelevant: true, requiresNumbnessOrTingling: true },
-    { id: 8, number: '2', text: "Do you wake up because of pain in your wrist?" },
-    { id: 9, number: '3', text: "Do you drop small objects like coins or a cup?" },
-    { id: 10, number: '4', text: "Do you often have neck pain?" },
-    { id: 11, number: '5', text: "Do you have numbness or tingling in your toes?" },
-    { id: 12, number: '6', text: "Have your symptoms improved with using wrist support brace or splint? (If relevant)", hasNotRelevant: true }
+    { id: 7, number: '2', text: "Did you have numbness or tingling in your fingers when you were pregnant? (If relevant)", hasNotRelevant: true },
+    { id: 8, number: '3', text: "Do you wake up because of pain in your wrist?" },
+    { id: 9, number: '4', text: "Do you drop small objects like coins or a cup?" },
+    { id: 10, number: '5', text: "Do you often have neck pain?" },
+    { id: 11, number: '6', text: "Do you have numbness or tingling in your toes?" },
+    { id: 12, number: '7', text: "Have your symptoms improved with using wrist support brace or splint? (If relevant)", hasNotRelevant: true }
   ];
