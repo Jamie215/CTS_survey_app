fa6e1df09a5c318c9047f215e0e49979a0c83c3c
Jamie Kim <jmkim0603@gmail.com>
2026-07-15 13:28:05 -0400

    Shifting datetime format

--- hooks/useExport.js
+++ hooks/useExport.js
@@ -206,7 +206,7 @@ export function useExport({
 }) {
   const date = new Date();
   const formattedDate = new Intl.DateTimeFormat('en-US', {
-    day: '2-digit', month: '2-digit', year: 'numeric'}).format(date).replace(/\//g, '-');
+    year: 'numeric', month: '2-digit', day: '2-digit'}).format(date).replace(/\//g, '-');
   const handleExportCSV = useCallback(() => {
     const data = {
       timestamp: formattedDate,
